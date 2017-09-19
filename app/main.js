d46abbbe641c5a37cbdae216bd95cdc019182a93
Martin Feineis <martinfeineis@gmx.de>
2017-09-19 12:08:03 +0200

    Event handling with decoders

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-import { Cmd, Sub, programWithFlags, tag } from "../lib/elmish.js";
+import { Cmd, Decoder, Sub, programWithFlags, tag } from "../lib/elmish.js";
 
 import prop from "../lib/ramda/src/prop.js";
 import { tmpl } from "./tmpl.js";
@@ -8,21 +8,29 @@ const div = tag("div");
 const footer = tag("footer");
 const header = tag("header");
 const hr = tag("hr");
+const input = tag("input");
 const main = tag("main");
 const style = tag("style");
 
-const mainView = ({ third }) =>
+const Msg = {
+    search: "SEARCH",
+    someButton: "SOME_BUTTON",
+    toggleHeader: "TOGGLE_HEADER",
+};
+
+const mainView = ({ third, search }) =>
       main(null, [
+          input({ onInput: [Msg.search, Decoder.targetValue], value: search }),
           third,
       ]);
 
-const topView = ({ first, second, third }) => [
-    header({ class: "--sticky", onClick: () => console.log("header sideeffect!") }, [
+const topView = ({ first, second, third, search }) => [
+    header({ class: "--sticky", onClick: [Msg.toggleHeader, second] }, [
         first,
-        button({ onClick: () => console.log("button sideeffect!") }, "Boom!"),
+        button({ onClick: [Msg.someButton] }, "Boom!"),
     ]),
     //second,
-    mainView({ third }),
+    mainView({ third, search }),
 ];
 
 const bottomView = ({ fourth }) =>
@@ -32,11 +40,7 @@ const bottomView = ({ fourth }) =>
       ]);
 
 const view = model => [
-    style({ type: "text/css" }, `
-
-        body { margin: 0; padding: 0; }
-
-    `),
+    style({ type: "text/css" }, ` body { margin: 0; padding: 0; } `),
     topView(model),
     bottomView(model),
 ];
@@ -46,6 +50,7 @@ const init = flags => [{
     second: 2,
     third: 3,
     fourth: 4,
+    search: "",
 }, Cmd.none];
 const subscriptions = model => Sub.none;
 const update = (msg, model) => [model, Cmd.none];
